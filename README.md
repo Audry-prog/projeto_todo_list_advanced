c5291713d950e1f0506c2fab78faf71042b5358a
Audry Ávalos <audryavalos@hotmail.com>
2025-02-04 23:40:46 -0300

    Uploading the complete project to Github

---
+++ README.md
@@ -0,0 +1,79 @@
+# 📋 Todo List Advanced
+
+Bem-vindo ao **Todo List Advanced**! Este projeto foi desenvolvido com base na videoaula de **Matheus Battisti** onde exploramos técnicas e tecnologias para criar uma aplicação web de lista de tarefas avançada, com funcionalidades como adicionar, editar, excluir e marcar tarefas como concluídas, além de utilizar o armazenamento local para persistência de dados.
+
+Você pode assistir à videoaula [aqui](https://www.youtube.com/watch?v=HSssE1PRQcA).
+
+---
+
+## 📌 Sobre o Projeto
+
+O **Todo List Advanced* é uma aplicação de lista de tarefas com as seguintes funcionalidades:
+
+- ✅ **Adicionar tarefas**: Permite adicionar novas tarefas à lista.
+- ✏️ **Editar tarefas**: Editar tarefas já adicionadas.
+- ✅ **Marcar como concluído**: Tarefas podem ser marcadas como feitas.
+- ❌ **Remover tarefas**: Tarefas podem ser removidas da lista.
+- 🔍 **Pesquisar tarefas**: Filtro para buscar tarefas específicas.
+- ⚙️ **Filtrar tarefas**: Filtros para exibir todas, feitas ou pendentes.
+- 💾 **Armazenamento local**: As tarefas são salvas no `localStorage`, garantindo persistência entre as sessões.
+
+---
+
+## 🚀 Tecnologias Utilizadas
+
+- **HTML5** → Estrutura da página.
+- **CSS3** → Estilização da interface e layout.
+- **JavaScript** → Lógica da aplicação, interatividade e manipulação do `localStorage`.
+- **Font Awesome** → Ícones utilizados para ações (adicionar, editar, concluir, excluir).
+- **localStorage** → API de armazenamento no navegador que permite salvar as tarefas entre as sessões.
+
+---
+
+## 📂 Estrutura do Projeto
+
+````
+projeto-todo-list-advanced/
+│
+├── css/
+│   └── styles.css      # Estilos da aplicação
+├── img/                # Imagens do projeto
+├── js/
+│   └── scripts.js      # Lógica e interatividade da aplicação
+├── index.html          # Estrutura HTML da página
+└── README.md           # Documentação do projeto
+````
+
+
+## 📡 Como Executar o Projeto
+
+1. **Clone o repositório**:
+
+1. Clone o repositório:
+   ```sh
+   git clone https://github.com/Audry-prog/projeto-todo-list-advanced.git
+   ```
+2. Acesse a pasta do projeto:
+   ```sh
+   cd projeto-todo-list-advance
+   ```
+
+3. Abra o arquivo HTML:
+Basta abrir o arquivo index.html no seu navegador para visualizar a aplicação funcionando.
+
+## 📢 Funcionalidades
+
+✔️ Adicionar tarefa: Adicione uma nova tarefa digitando no campo de entrada.
+✏️ Editar tarefa: Clique no ícone de edição para modificar o texto de uma tarefa.
+✅ Marcar tarefa como concluída: Clique no ícone de checkmark para marcar a tarefa como concluída.
+❌ Remover tarefa: Exclua tarefas com o ícone de remoção.
+🔍 Pesquisar tarefas: Busque tarefas digitando no campo de pesquisa.
+⚙️ Filtrar tarefas: Filtre tarefas por status: "Todas", "Feitas" ou "A Fazer".
+
+📷 Print da Tela do Projeto
+![Print da Tela do Projeto](img/print_tela_projeto_todo_list)
+
+📜 Licença
+Este projeto foi desenvolvido para fins educacionais com base na videoaula de Matheus Battisti. Sinta-se à vontade para explorar, adaptar e contribuir com o projeto. 🎉
+
+💡 Dúvidas ou sugestões? Contribua com o projeto ou entre em contato! 😃
---
+++ js/scripts.js
@@ -0,0 +1,254 @@
+/* The Logic will occur in 4 parts: Selection of Elements, Functions, Events and Local data storage */
+
+/* Selection of Elements */
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+const editForm = document.querySelector("#edit-form");
+const editInput = document.querySelector("#edit-input");
+const cancelEditBtn = document.querySelector("#cancel-edit-btn");
+const searchInput = document.querySelector("#search-input");
+const eraseBtn = document.querySelector("#erase-button");
+const filterBtn = document.querySelector("#filter-select");
+
+let oldInputValue; /* Variable to store the old value of the input field */
+
+/* Functions */
+const saveTodo = (text, done = 0, save = true) => {
+  const todo = document.createElement("div");
+  todo.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = text;
+  todo.appendChild(todoTitle);
+
+  /* Create the Done Button */
+  const doneBtn = document.createElement("button");
+  doneBtn.classList.add("finish-todo");
+  doneBtn.innerHTML = '<i class="fa-solid fa-check"></i>';
+  todo.appendChild(doneBtn);
+
+  /* Create the Edit Button */
+  const editBtn = document.createElement("button");
+  editBtn.classList.add("edit-todo");
+  editBtn.innerHTML = '<i class="fa-solid fa-pen"></i>';
+  todo.appendChild(editBtn);
+
+  /* Create the Delet Button */
+  const deletBtn = document.createElement("button");
+  deletBtn.classList.add("remove-todo");
+  deletBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
+  todo.appendChild(deletBtn);
+
+  /* Using data from localStorage */
+  if (done) {
+    todo.classList.add("done");
+  }
+
+  if (save) {
+    saveTodoLocalStorage({ text, done });
+  }
+
+  todoList.appendChild(todo); /* Append the item todo to the todoList */
+
+  todoInput.value =
+    ""; /* Clear the input field after the user types in the field */
+  todoInput.focus(); /* Focus the input field after the user types in the field */
+};
+
+const toggleForms = () => {
+  editForm.classList.toggle("hide");
+  todoForm.classList.toggle("hide");
+  todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+  const allTodos = document.querySelectorAll(".todo");
+  allTodos.forEach((todo) => {
+    let todoTitle = todo.querySelector("h3");
+    if (todoTitle.innerText.trim() === oldInputValue) {
+      todoTitle.innerText = text;
+
+      /* Using data from localStorage */
+      updateTodoLocalStorage(oldInputValue, text);
+    }
+  });
+};
+
+const getSearchedTodos = (search) => {
+  const todos = document.querySelectorAll(".todo");
+
+  todos.forEach((todo) => {
+    const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+    todo.style.display = "flex";
+
+    console.log(todoTitle);
+
+    if (!todoTitle.includes(search)) {
+      todo.style.display = "none";
+    }
+  });
+};
+
+const filterTodos = (filterValue) => {
+  const todos = document.querySelectorAll(".todo");
+
+  switch (filterValue) {
+    case "all":
+      todos.forEach((todo) => (todo.style.display = "flex"));
+
+      break;
+
+    case "done":
+      todos.forEach((todo) =>
+        todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+
+      break;
+
+    case "todo":
+      todos.forEach((todo) =>
+        !todo.classList.contains("done")
+          ? (todo.style.display = "flex")
+          : (todo.style.display = "none")
+      );
+
+      break;
+
+    default:
+      break;
+  }
+};
+
+/* Events */
+todoForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inputValue = todoInput.value;
+
+  if (inputValue) {
+    saveTodo(inputValue);
+  }
+});
+
+document.addEventListener("click", (e) => {
+  const targetEl = e.target;
+  const parentEl = targetEl.closest("div");
+  let todoTitle;
+
+  if (parentEl && parentEl.querySelector("h3")) {
+    todoTitle = parentEl.querySelector("h3").innerText || "";
+  }
+
+  if (targetEl.classList.contains("finish-todo")) {
+    parentEl.classList.toggle("done");
+
+    updateTodoStatusLocalStorage(todoTitle);
+  }
+
+  if (targetEl.classList.contains("remove-todo")) {
+    parentEl.remove();
+
+    removeTodoLocalStorage(todoTitle);
+  }
+
+  if (targetEl.classList.contains("edit-todo")) {
+    toggleForms();
+
+    editInput.value =
+      todoTitle; /* Set the value of the input field to the todoTitle */
+    oldInputValue = todoTitle; /* Store the old value of the input field */
+  }
+});
+
+cancelEditBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  toggleForms();
+});
+
+editForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const editInputValue = editInput.value;
+  if (editInputValue) {
+    updateTodo(editInputValue);
+  }
+
+  toggleForms();
+});
+
+searchInput.addEventListener("keyup", (e) => {
+  const search = e.target.value;
+
+  getSearchedTodos(search);
+});
+
+eraseBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  searchInput.value = "";
+
+  searchInput.dispatchEvent(new Event("keyup"));
+});
+
+filterBtn.addEventListener("change", (e) => {
+  const filterValue = e.target.value;
+
+  filterTodos(filterValue);
+});
+
+/* Local data storage */
+const getTodosLocalStorage = () => {
+  const todos = JSON.parse(localStorage.getItem("todos")) || [];
+
+  return todos;
+};
+
+const loadTodos = () => {
+  const todos = getTodosLocalStorage();
+
+  todos.forEach((todo) => {
+    saveTodo(todo.text, todo.done, 0);
+  });
+};
+
+const saveTodoLocalStorage = (todo) => {
+  const todos = getTodosLocalStorage();
+
+  todos.push(todo);
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const removeTodoLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+
+  const filteredTodos = todos.filter((todo) => todo.text.trim() != todoText.trim());
+
+  localStorage.setItem("todos", JSON.stringify(filteredTodos));
+};
+
+const updateTodoStatusLocalStorage = (todoText) => {
+  const todos = getTodosLocalStorage();
+
+  todos.map((todo) =>
+    todo.text === todoText ? (todo.done = !todo.done) : null
+  );
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+const updateTodoLocalStorage = (todoOldText, todoNewText) => {
+  const todos = getTodosLocalStorage();
+
+  todos.map((todo) =>
+    todo.text === todoOldText ? (todo.text = todoNewText) : null
+  );
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+};
+
+loadTodos();
